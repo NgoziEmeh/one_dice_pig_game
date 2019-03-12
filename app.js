a7db3812903c3156504e48f74a060754a8c72835
Ngozi Emeh-Festus <35117703+NgoziEmeh@users.noreply.github.com>
2019-03-12 14:42:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,5 +8,5 @@ init();
 
 //DOM manipulation
 document.querySelector('.btn-roll').addEventListener('click', function() {
-  
+  //1. Generate random number
   var dice = Math.floor(Math.random() * 6) + 1;
