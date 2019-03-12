95f92e417e5e42bd071452c5449b86a2c324e521
Ngozi Emeh-Festus <35117703+NgoziEmeh@users.noreply.github.com>
2019-03-12 14:57:32 +0100

    Update app.js

--- app.js
+++ app.js
@@ -31,3 +31,4 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
 
 document.querySelector('.btn-hold').addEventListener('click', function(){
+  //Add current score to main score
