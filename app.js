4993936f8075419ea13446353884ba27273273b2
Ngozi Emeh-Festus <35117703+NgoziEmeh@users.noreply.github.com>
2019-03-12 15:30:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -88,3 +88,4 @@ function init (){
   document.querySelector('.player-0-panel').classList.remove('winner');
   document.querySelector('.player-1-panel').classList.remove('winner');
   document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
