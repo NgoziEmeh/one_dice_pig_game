61a2de8f39d6f7b73eb5476ac15355e97e6ae746
Ngozi Emeh-Festus <35117703+NgoziEmeh@users.noreply.github.com>
2019-03-12 15:05:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -43,3 +43,5 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
     document.getElementById('current-0').textContent = 0;
     document.getElementById('current-1').textContent = 0;
     
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    
